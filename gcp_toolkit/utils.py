e5475d604ba6853ea222863776c3332c2c0a2d06
dougpm <douglaspmartins0@gmail.com>
2020-02-08 22:14:40 -0300

    client instance change

--- gcp_toolkit/utils.py
+++ gcp_toolkit/utils.py
@@ -1,9 +1,10 @@
 from google.cloud import storage
 
-def create_bucket_folder(bucket_name, folder_name, storage_client=storage.Client()):
+def create_bucket_folder(bucket_name, folder_name, storage_client=None):
 
     """Creates a Folder in Storage"""
-    
+    if storage_client is None:
+        storage_client = storage.Client()
     if '/' not in folder_name:
         folder_name = folder_name + '/'
     bucket = storage_client.get_bucket(bucket_name)

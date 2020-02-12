87239d6e0026d0279ab428827530f27196c3e692
dougpm <douglaspmartins0@gmail.com>
2020-02-11 22:10:37 -0300

    error handling and finally clauses

--- gcp_toolkit/io.py
+++ gcp_toolkit/io.py
@@ -8,40 +8,42 @@ import gcp_toolkit.utils as gtku
 #TODO: separate bq and storage classes
 
 class IO:
-    def __init__(self, bucket_name, bq_client=None, storage_client=None):
+    def __init__(self, bucket_name, staging_dataset, bq_client=None, storage_client=None):
         self.bq_client = bigquery.Client() if bq_client is None else bq_client
         self.storage_client = storage.Client() if storage_client is None else storage_client
+        self.bucket_name = bucket_name
+        self.staging_dataset = staging_dataset
         try:
             self.bucket = self.storage_client.get_bucket(bucket_name)
         except Exception as e:
             if '403' in str(e):
                 print("User doesn't have access to {}".format(bucket_name))
-            if '404' in str(e):
+            elif '404' in str(e):
                 print("Bucket {} doesn't exist".format(bucket_name))
             else:
                 print(e)
-    def bq_to_bucket(self, query, path_to_file, staging_dataset=None):
+        try:
+            self.bq_client.get_dataset(staging_dataset)
+        except Exception as e:
+            if '403' in str(e):
+                print("User doesn't have access to {}".format(staging_dataset))
+            elif '404' in str(e):
+                print("Dataset {} doesn't exist".format(staging_dataset))
+
+    def bq_to_bucket(self, query, path_to_file):
 
         """Runs query in BigQuery and stores results in Storage"""
 
-        if staging_dataset is None:
-            raise
-            #TODO: create random staging dataset function in utils, call it in constructor
-
         bucket_file_url = 'gs://{}/{}'.format(self.bucket_name, path_to_file)
         letters = string.ascii_lowercase
-        staging_table = '{}.{}.temp_table'.format(self.bq_client.project, staging_dataset) + ''.join(random.choice(letters) for i in range(100))
+        staging_table = '{}.{}.temp_table_'.format(self.bq_client.project, self.staging_dataset) + ''.join(random.choice(letters) for i in range(100))
         
         job_config = bigquery.QueryJobConfig()
         job_config.destination = staging_table
         query_job = self.bq_client.query(query, job_config=job_config)
-
         query_job.result()
-
         extract_job = self.bq_client.extract_table(staging_table, bucket_file_url)
         extract_job.result()
-
-        #TODO: finally statement or context manager
         self.bq_client.delete_table(staging_table)
 
     def bucket_to_df(self, path_to_file):
@@ -50,8 +52,7 @@ class IO:
 
         df = pd.DataFrame()
         partial_df = pd.DataFrame()
-        bucket = self.storage_client.get_bucket(self.bucket_name)
-        blobs = bucket.list_blobs()
+        blobs = self.bucket.list_blobs()
         for blob in blobs:
             if path_to_file in blob.name:
                 url = 'gs://{}/{}'.format(self.bucket_name, blob.name)
@@ -61,8 +62,7 @@ class IO:
         
         return df
 
-
-    def bq_to_df(self, query, staging_dataset=None, use_builtin=False):
+    def bq_to_df(self, query, use_builtin=False):
         
         """Runs a query in BigQuery and loads the results into a pandas Data Frame"""
         
@@ -70,23 +70,16 @@ class IO:
             query_job = self.bq_client.query(query)  
             return query_job.result().to_dataframe()
 
-        if staging_dataset is None:
-            raise
-            #TODO: create random staging dataset function in utils, call it in constructor
-
         #it takes way too long to load very large tables with to_dataframe(), so doing the following is faster:
-
         letters = string.ascii_lowercase
         staging_blob = ''.join(random.choice(letters) for i in range(100)) + '/'
         gtku.create_bucket_folder(self.bucket_name, staging_blob)
-        print('Created {}'.format(staging_blob))
-        
-        self.bq_to_bucket(query, 'gs://{}/{}results*'.format(self.bucket_name, staging_blob), staging_dataset)
-        df = self.bucket_to_df('{}results'.format(staging_blob))
-
-        #TODO: finally statement or context manager
-        bucket = self.storage_client.get_bucket(self.bucket_name)
-        bucket.delete_blob(staging_blob)
+        try:
+            self.bq_to_bucket(query, 'gs://{}/{}results*'.format(self.bucket_name, staging_blob))
+            df = self.bucket_to_df('{}results'.format(staging_blob))
+        finally:
+            bucket = self.storage_client.get_bucket(self.bucket_name)
+            bucket.delete_blob(staging_blob)
 
         return df
 
@@ -98,7 +91,4 @@ class IO:
         job_config = bigquery.LoadJobConfig(schema=schema)
         job_config.write_disposition = bigquery.WriteDisposition.WRITE_TRUNCATE
         job = self.bq_client.load_table_from_dataframe(df, table_id, job_config=job_config)
-        
         job.result()
-
-

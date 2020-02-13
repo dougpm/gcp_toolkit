0f5bf0e28da46fca544e35caa8a7ed0cdcbf76b7
dougpm <douglaspmartins0@gmail.com>
2020-02-13 17:17:05 -0300

    fixed init

--- gcp_toolkit/__init__.py
+++ gcp_toolkit/__init__.py
@@ -2,5 +2,7 @@ from gcp_toolkit.io import (
     IO
 )
 from gcp_toolkit.utils import (
-    create_bucket_folder
+    create_bucket_folder,
+    convert_pandas_gbq_schema,
+    change_table_schema
 )
